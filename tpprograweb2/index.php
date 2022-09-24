473390c54ab5d493e82475c6eb91f40edd8b63ef
rommignoatto <rommignoatto@gmail.com>
2022-09-24 19:55:08 -0300

    Se incorporaron imagenes en index y products

--- tpprograweb2/index.php
+++ tpprograweb2/index.php
@@ -1,102 +1,104 @@
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-    <?php require('layouts/_css.php') ?>
-    <title>Food store</title>
-</head>
-<body>
-    <header>
-    <?php require('layouts/_headercito.php') ?> 
-    <?php require('layouts/_nav.php') ?>
-    </header>
-    <main>
-    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://picsum.photos/1920/1080" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="https://picsum.photos/1920/1080" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="https://picsum.photos/1920/1080" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-<div>
-    <h1>SOMOS UNA HAMBURGUESERIA</h1>
-    <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptates rerum animi? Animi nemo iste rerum. Commodi cumque sapiente nostrum minus inventore at expedita odit ex, quae necessitatibus tenetur doloremque!locale_filter_matches
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam maiores nobis, voluptatem dignissimos in accusantium necessitatibus blanditiis sed repudiandae accusamus voluptatum possimus? Nobis temporibus nostrum iure officia, ab laborum adipisci.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe explicabo illo vero illum natus quis non excepturi adipisci voluptatibus eveniet, dicta provident voluptas perspiciatis quae eligendi incidunt, officiis eum labore!
-    </h6>
-</div>
-<div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col">
-    <div class="card">
-      <img src="https://picsum.photos/50" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="https://picsum.photos/50" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="https://picsum.photos/50" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-      </div>
-    </div>
-  </div>
-</div>
-<div>
-<h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptates rerum animi? Animi nemo iste rerum. Commodi cumque sapiente nostrum minus inventore at expedita odit ex, quae necessitatibus tenetur doloremque!locale_filter_matches
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam maiores nobis, voluptatem dignissimos in accusantium necessitatibus blanditiis sed repudiandae accusamus voluptatum possimus? Nobis temporibus nostrum iure officia, ab laborum adipisci.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe explicabo illo vero illum natus quis non excepturi adipisci voluptatibus eveniet, dicta provident voluptas perspiciatis quae eligendi incidunt, officiis eum labore!
-    </h6>  
-</div>
 
-    </main>
-    <?php require('layouts/_footer.php') ?>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  <?php require('layouts/_css.php') ?>
+  <title>Food store</title>
+</head>
+
+<body>
+  <header>
+    <?php require('layouts/_headercito.php') ?>
+    <?php require('layouts/_nav.php') ?>
+  </header>
+  <main>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <?php echo  '<img src="./layouts/images/hamb-banner.jpg" class="d-block w-100" alt="...">' ?>
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Las mejores hamburguesas</h5>
+            <p>Hechas en los locales todos los días</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <?php echo  '<img src="./layouts/images/fries-banner.jpg" class="d-block w-100" alt="...">' ?>
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Cortadas a manos</h5>
+            <p>Las papas fritas más crocantes</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <?php echo  '<img src="./layouts/images/beer-banner.jpg" class="d-block w-100" alt="...">' ?>
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Cerveza tirada</h5>
+            <p>Más de 15 canillas con las mejores cervezas artesanales</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    <div>
+      <h1>SOMOS UNA HAMBURGUESERIA</h1>
+      <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptates rerum animi? Animi nemo iste rerum. Commodi cumque sapiente nostrum minus inventore at expedita odit ex, quae necessitatibus tenetur doloremque!locale_filter_matches
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam maiores nobis, voluptatem dignissimos in accusantium necessitatibus blanditiis sed repudiandae accusamus voluptatum possimus? Nobis temporibus nostrum iure officia, ab laborum adipisci.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe explicabo illo vero illum natus quis non excepturi adipisci voluptatibus eveniet, dicta provident voluptas perspiciatis quae eligendi incidunt, officiis eum labore!
+      </h6>
+    </div>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card">
+          <img src="https://picsum.photos/50" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://picsum.photos/50" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://picsum.photos/50" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptates rerum animi? Animi nemo iste rerum. Commodi cumque sapiente nostrum minus inventore at expedita odit ex, quae necessitatibus tenetur doloremque!locale_filter_matches
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam maiores nobis, voluptatem dignissimos in accusantium necessitatibus blanditiis sed repudiandae accusamus voluptatum possimus? Nobis temporibus nostrum iure officia, ab laborum adipisci.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe explicabo illo vero illum natus quis non excepturi adipisci voluptatibus eveniet, dicta provident voluptas perspiciatis quae eligendi incidunt, officiis eum labore!
+      </h6>
+    </div>
+
+  </main>
+  <?php require('layouts/_footer.php') ?>
 </body>
 
 </html>
--- tpprograweb2/products.php
+++ tpprograweb2/products.php
@@ -1,64 +1,63 @@
 <?php
 
 $lista_menu = array(
-	array('codigo' => 1, 'nombre' => 'Pizza con jamón y huevo', 'categoria' => 'Pizzas', 'precio' => 2000, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
-	array('codigo' => 2, 'nombre' => 'Pizza napolitana', 'categoria' => 'Pizzas', 'precio' => 2500, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
-	array('codigo' => 3, 'nombre' => 'Pizza caprese', 'categoria' => 'Pizzas', 'precio' => 2700, 'descuento' => 500, 'descripcion' => 'carne,cheddar,panceta,huevo'),
-	array('codigo' => 4, 'nombre' => 'Ensalada caesar', 'categoria' => 'Ensaladas', 'precio' => 800, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
-	array('codigo' => 5, 'nombre' => 'Ensalada rusa', 'categoria' => 'Ensaladas', 'precio' => 500, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
-	array('codigo' => 6, 'nombre' => 'Hamburguesa simple', 'categoria' => 'Hamburguesas', 'precio' => 900, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
-	array('codigo' => 7, 'nombre' => 'Hamburguesa completa', 'categoria' => 'Hamburguesas', 'precio' => 1700, 'descuento' => 400, 'descripcion' => 'carne,cheddar,panceta,huevo'),
-	array('codigo' => 8, 'nombre' => 'Coca cola', 'categoria' => 'Bebidas', 'precio' => 300, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
-	array('codigo' => 9, 'nombre' => 'Fanta', 'categoria' => 'Bebidas', 'precio' => 300, 'descuento' => 20, 'descripcion' => 'carne,cheddar,panceta,huevo'),
-	array('codigo' => 10, 'nombre' => 'Agua mineral', 'categoria' => 'Bebidas', 'precio' => 150, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
-	array('codigo' => 11, 'nombre' => 'Helado', 'categoria' => 'Postres', 'precio' => 550, 'descuento' => 50, 'descripcion' => 'carne,cheddar,panceta,huevo'),
-	array('codigo' => 12, 'nombre' => 'Flan casero', 'categoria' => 'Postres', 'precio' => 400, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
+  array('codigo' => 1, 'nombre' => 'Pizza con jamón y huevo', 'numero' => '1', 'precio' => 2000, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
+  array('codigo' => 2, 'nombre' => 'Pizza napolitana', 'numero' => '2', 'precio' => 2500, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
+  array('codigo' => 3, 'nombre' => 'Pizza caprese', 'numero' => '3', 'precio' => 2700, 'descuento' => 500, 'descripcion' => 'carne,cheddar,panceta,huevo'),
+  array('codigo' => 4, 'nombre' => 'Ensalada caesar', 'numero' => '4', 'precio' => 800, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
+  array('codigo' => 5, 'nombre' => 'Ensalada rusa', 'numero' => '5', 'precio' => 500, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
+  array('codigo' => 6, 'nombre' => 'Hamburguesa simple', 'numero' => '6', 'precio' => 900, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
+  array('codigo' => 7, 'nombre' => 'Hamburguesa completa', 'numero' => '7', 'precio' => 1700, 'descuento' => 400, 'descripcion' => 'carne,cheddar,panceta,huevo'),
+  array('codigo' => 8, 'nombre' => 'Coca cola', 'numero' => '8', 'precio' => 300, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
+  array('codigo' => 9, 'nombre' => 'Fanta', 'numero' => '9', 'precio' => 300, 'descuento' => 20, 'descripcion' => 'carne,cheddar,panceta,huevo'),
+  array('codigo' => 10, 'nombre' => 'Agua mineral', 'numero' => '10', 'precio' => 150, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
+  array('codigo' => 11, 'nombre' => 'Helado', 'numero' => '11', 'precio' => 550, 'descuento' => 50, 'descripcion' => 'carne,cheddar,panceta,huevo'),
+  array('codigo' => 12, 'nombre' => 'Flan casero', 'numero' => '12', 'precio' => 400, 'descuento' => 0, 'descripcion' => 'carne,cheddar,panceta,huevo'),
 );
 
 ?>
 
-<html lang="en"
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<html lang="en" <head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-    <title>Productos</title>
-    <?php require('layouts/_css.php') ?>
+<title>Productos</title>
+<?php require('layouts/_css.php') ?>
 </head>
-<body>
-<header>
-<?php require('layouts/_headercito.php') ?> 
-<?php require('layouts/_nav.php') ?>
-</header>
-<main>
-<div class="row row-cols-1 row-cols-md-3 g-4">
-<?php foreach ($lista_menu as $lista) : ?>
-<div class="col">
-    <div class="card" style="width:300px">
-      <img src= "https://picsum.photos/15" class="card-img-top" alt="<?php echo $lista['categoria'] ?>">
-      <div class="card-body">
-        <h5 class="card-title"><?php echo $lista['nombre'] ?></h5>
-        <p><?php echo $lista['descripcion']?></p>
-        <strong><p class="card-text"><?php echo "$". $lista['precio']?></p></strong>
-      </div>
-    </div>
-</div>
-<?php endforeach ?>
-</div>
-</main>
-<footer>
-<?php require('layouts/_footer.php') ?>
 
-</footer>
+<body>
+  <header>
+    <?php require('layouts/_headercito.php') ?>
+    <?php require('layouts/_nav.php') ?>
+  </header>
+  <main>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <?php foreach ($lista_menu as $lista) : ?>
+        <div class="col">
+          <div class="card" style="width:300px">
+            <img src="./layouts/images/<?php echo $lista['numero'] ?>.jpg" class=" card-img-top" alt="<?php echo $lista['numero'] ?>">
+            <div class="card-body">
+              <h5 class="card-title"><?php echo $lista['nombre'] ?></h5>
+              <p><?php echo $lista['descripcion'] ?></p>
+              <strong>
+                <p class="card-text"><?php echo "$" . $lista['precio'] ?></p>
+              </strong>
+            </div>
+          </div>
+        </div>
+      <?php endforeach ?>
+    </div>
+  </main>
+  <footer>
+    <?php require('layouts/_footer.php') ?>
+
+  </footer>
 
 
 
 
 </body>
+
 </html>
-
-
-
-
